0f6627c5d5a96f5bf925380dad0db0f70e8a5fd1
Sok Heng <44894784+SokunHeng@users.noreply.github.com>
2019-08-07 10:50:07 +0700

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -7,31 +7,25 @@ operatr = [
 ]
 for row in operatr:
     print(row)
-a = 1
-while a <= 2:
-    try:
-        print("Calculator program")
-        n1 = float(input(": "))
-        s = input("Operator: ")
-        n2 = float(input(": "))
-        if s == "+":
-            e1 = n1 + n2
-            print(f"result: {e1}")
-        elif s == "-":
-            e2 = n1 - n2
-            print(f"result: {e2}")
-        elif s == "/":
-            e3 = n1 / n2
-            print(f"result: {e3}")
-        elif s == "*":
-            e4 = n1 * n2
-            print(f"result: {e4}")
-        elif s == "%":
-            e = n1 % n2
-            print(f"result: {e}")
+try:
+    a = 1
+    while a < 2:
+        n1 = float(input("Number: "))
+        op = input("Enter Operator: ")
+        n2 = float(input("Number: "))
+        if op == "+":
+            re = n1 + n2
+            print(f'The result is {re}')
+        elif op == "-":
+            re = n1 - n2
+            print(f'The result is {re}')
+        elif op == "*":
+            re = n1 * n2
+            print(f'The result is {re}')
+        elif op == "/":
+            re = n1 / n2
+            print(f'The result is {re}')
         else:
-            print("Invalid input!")
-    except ZeroDivisionError:
-        print("Number can not be 0.")
-    except ValueError:
-        print("Invalid value")
+            print("Invalid input! Please try again.")
+except ZeroDivisionError:
+    print("The result is 0")
