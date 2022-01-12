2592c723bb86f4f576f229d6999b3d019606669b
Sok Heng <snamheng@gmail.com>
2022-01-13 01:10:28 +0700

    Update Calculator.py
    Added Color feature.

--- Calculator.py
+++ Calculator.py
@@ -1,31 +1,54 @@
-print("Calculaor program.")
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+"""
+ => Old Version
+
 operatr = [
-    "+ = Addition",
-    "- = Subtraction",
-    "/ = Division",
-    "* = Multiplication"
+    "         + = Addition",
+    "         - = Subtraction",
+    "         / = Division",
+    "         * = Multiplication"
 ]
 for row in operatr:
     print(row)
+"""
+
+print(f"{Fore.BLUE}======================================")
+print(f"     {Fore.GREEN}Welcome To Calculaor Program.")
+print(f"          {Fore.BLUE}+ {Fore.WHITE}= Addition")
+print(f"          {Fore.GREEN}- {Fore.WHITE}= Subtraction")
+print(f"          {Fore.MAGENTA}/ {Fore.WHITE}= Division ")
+print(f"          {Fore.RED}* {Fore.WHITE}= Multiplication")
+print(f"{Fore.BLUE}======================================")
+print(f"{Fore.BLUE}=   {Fore.WHITE}Input your number to calculate {Fore.BLUE}  =")
+print(f"{Fore.BLUE}======================================")
 try:
     a = 1
     while a < 2:
-        n1 = float(input("Number: "))
-        op = input("Enter Operator: ")
-        n2 = float(input("Number: "))
+        n1 = float(input("Input Number: "))
+        op = input(f"Enter Operator: ")
+        n2 = float(input(f"Input Number: "))
         if op == "+":
             re = n1 + n2
-            print(f'The result is {re}')
+            print(f'Result {Fore.YELLOW}===> {Fore.WHITE}{re}')
+            print(f"{Fore.BLUE}======================================")
         elif op == "-":
             re = n1 - n2
-            print(f'The result is {re}')
+            print(f'Result {Fore.YELLOW}===> {Fore.WHITE}{re}')
+            print(f"{Fore.BLUE}======================================")
         elif op == "*":
             re = n1 * n2
-            print(f'The result is {re}')
+            print(f'Result {Fore.YELLOW}===> {Fore.WHITE}{re}')
+            print(f"{Fore.BLUE}======================================")
         elif op == "/":
             re = n1 / n2
-            print(f'The result is {re}')
+            print(f'Result {Fore.YELLOW}===> {Fore.WHITE}{re}')
+            print(f"{Fore.BLUE}======================================")
         else:
-            print("Invalid input! Please try again.")
+            print(f"{Fore.RED}Invalid input! Please try again!")
+            print(f"{Fore.BLUE}======================================")
 except ZeroDivisionError:
     print("The result is 0")
+    print(f"{Fore.BLUE}======================================")
